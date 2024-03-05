982c9921297c4492deb90de2d13f7be95fa722ec
Toby <tobybrown1996@gmail.com>
2024-03-05 16:03:20 -0500

    New html to replace header

--- index.php
+++ index.php
@@ -3,12 +3,27 @@
   //header('Location: public/');
   $page_title = 'Home';
   //include(SHARED_PATH . '/salamander-header.php');?>
+<?php
+  if(!isset($pageTitle)) { 
+    $pageTitle = 'Salamanders'; 
+  }
+?>
 
-  <h1>Southern Appalachian Salamanders</h1>
-  <p>This is the home page for Southern Appalachian Salamanders.</p>
+<!doctype html>
 
-  <ul>
-    <li><a href="<?php echo urlFor('../public/index.php'); ?>">SAS</a></li>
-  </ul>
+<html lang="en">
+  <head>
+    <title>SAS - <?php echo h($pageTitle); ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="<?php echo urlFor('/stylesheets/salamanders.css'); ?>" />
+  </head>
+
+  <body>
+    <h1>Southern Appalachian Salamanders</h1>
+    <p>This is the home page for Southern Appalachian Salamanders.</p>
+
+    <ul>
+      <li><a href="<?php echo urlFor('../public/index.php'); ?>">SAS</a></li>
+    </ul>
 
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
